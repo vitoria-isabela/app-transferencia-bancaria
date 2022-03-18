fd8f6c0ddd2b4bf977959194ae3978d2b2ba9a6a
vitoria-isabela <vitoriaisabela368@gmail.com>
2022-03-17 21:49:11 -0300

    Fix the AccountType Enum.

---
+++ dio-dotnet-poo-lab-1-master/Enums/AccountType.cs
@@ -0,0 +1,11 @@
+namespace DIO.Bank
+{
+    /// <summary>
+    /// Defines the account type
+    /// </summary>
+    public enum AccountType
+    {
+        NaturalPerson,
+        LegalPerson
+    }
+}
